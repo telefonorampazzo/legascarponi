fd2ec223fe4a3626337a3cabd4a310be7d3a0061
telefonorampazzo <telefonorampazzo@gmail.com>
2026-05-26 14:29:19 +0000

    Fix squad page module

---
+++ scarponi-remastered/app/squadre/[slug]/page.tsx
@@ -0,0 +1,9 @@
+export default function TeamPage() {
+  return (
+    <main className="min-h-screen bg-black text-white flex items-center justify-center">
+      <h1 className="text-5xl font-bold">
+        Pagina Squadra
+      </h1>
+    </main>
+  )
+}
